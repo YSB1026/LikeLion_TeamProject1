84fba4b28bfd22063e1d032c07e519978f08bbdb
YSB1026 <103161220+YSB1026@users.noreply.github.com>
2025-03-19 19:58:42 +0900

    25/03/19 플레이어 애니메이션
    blend tree로 idle, walk까지 했음,
    cinemachine 카메라 사용

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,34 +4,61 @@ public class Player : Character
 {
     Rigidbody2D rb;
     Collider2D collider;
-    float moveX, moveY;
+    Animator animator;
     float speed = 5f;
+    float projectileSpeed = 15f;
+    float moveX, moveY, lastMoveX = 1f, lastMoveY = 0f; // 마지막 입력 방향 (Idle 전환 시 유지)
+    bool isMoving;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         collider = GetComponent<Collider2D>();
+        animator = GetComponent<Animator>();
     }
     void Start()
     {
         
     }
-
     void Update()
     {
-        (moveX, moveY) = (Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
+        Move();
+        SetAnimParams();
     }
     private void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2(moveX, moveY) * speed;
+
     }
     protected override void Death()
     {
-        throw new System.NotImplementedException();
+        //플레이어 사망처리
     }
 
     protected override void Move()
     {
-        throw new System.NotImplementedException();
+        //플레이어 움직임
+        (moveX, moveY) = (Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        transform.Translate(new Vector2(moveX, moveY).normalized * speed * Time.deltaTime);
+    }
+    private void SetAnimParams()
+    {
+        isMoving = moveX != 0 || moveY != 0;
+        if (isMoving)//키를 땠을때 어느 방향을 보고있는지 저장하기 위한 로직
+        {
+            if (Mathf.Abs(moveX) > Mathf.Abs(moveY)) moveY = 0;
+            else moveX = 0;
+            lastMoveX = moveX;
+            lastMoveY = moveY;
+        }
+
+        animator.SetFloat("dirX", isMoving ? moveX : lastMoveX);
+        animator.SetFloat("dirY", isMoving ? moveY : lastMoveY);
+        animator.SetBool("isWalking", isMoving);
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //회복처리
+        //아이템, 경험치 처리
+        //
+    }
 }
