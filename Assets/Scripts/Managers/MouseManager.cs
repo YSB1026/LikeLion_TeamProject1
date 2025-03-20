760d8730617a7b3c91aedc4e216e67b578a68866
YSB1026 <103161220+YSB1026@users.noreply.github.com>
2025-03-20 18:45:46 +0900

    25/03/20 mouse cursor, 파일정리

---
+++ Assets/Scripts/Managers/MouseManager.cs
@@ -0,0 +1,41 @@
+using Singleton.Component;
+using UnityEngine;
+
+
+public class MouseManager : SingletonComponent<MouseManager>
+{
+    [SerializeField] Transform player;
+    [SerializeField] float threshold;//threshold -> clamp(min,max) 줄 값.
+
+    #region Singleton
+    protected override void AwakeInstance()
+    {
+
+    }
+
+    protected override bool InitInstance()
+    {
+        return true;
+    }
+
+    protected override void ReleaseInstance()
+    {
+
+    }
+    #endregion
+
+    void Update()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+        //Vector3 target = -mousePos; //나중에 알아보기 왜 반대로 되는지, 느낌상 z값이 음수라서 그런거같음.
+        //target.x = Mathf.Clamp(target.x, -threshold + player.position.x, threshold + player.position.x);
+        //target.y = Mathf.Clamp(target.y, -threshold + player.position.y, threshold + player.position.y);
+        //mousePos = -mousePos;
+        transform.position = mousePos;
+    }
+    public Quaternion GetRotationInfo()
+    {
+        //Debug.Log(Quaternion.AngleAxis(angle - 90, Vector3.forward));
+        return Quaternion.AngleAxis(90, Vector3.forward);
+    }
+}
