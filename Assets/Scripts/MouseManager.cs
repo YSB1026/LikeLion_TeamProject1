3be6c484822e355492a0f9dbaa4e903073714185
Mitaro900 <jhkim9622@gmail.com>
2025-03-20 18:15:47 +0900

    Modify MouseManager
    마우스매니저 수정

--- Assets/Scripts/MouseManager.cs
+++ Assets/Scripts/MouseManager.cs
@@ -1,38 +1,37 @@
+using Singleton.Component;
 using Unity.Cinemachine;
 using UnityEngine;
 
-public class MouseManager : MonoBehaviour
+public class MouseManager : SingletonComponent<MouseManager>
 {
     [SerializeField] Transform player;
-    [SerializeField] Camera cam;
     [SerializeField] float threshold;//threshold -> clamp(min,max) 줄 값.
-    public static MouseManager Instance { get; private set; }
 
-    private void Awake()
+    #region Singleton
+    protected override void AwakeInstance()
     {
-        if(Instance == null)
-        {
-            Instance = this;
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-
-        if(cam == null)
-        {
-            Debug.LogError("메인 카메라를 찾을 수 없습니다.");
-        }
+        
     }
+
+    protected override bool InitInstance()
+    {
+        return true;
+    }
+
+    protected override void ReleaseInstance()
+    {
+        
+    }
+    #endregion
 
     void Update()
     {
-        Vector3 mousePos = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Mathf.Abs(cam.transform.position.z)));
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
         //Vector3 target = -mousePos; //나중에 알아보기 왜 반대로 되는지, 느낌상 z값이 음수라서 그런거같음.
         //target.x = Mathf.Clamp(target.x, -threshold + player.position.x, threshold + player.position.x);
         //target.y = Mathf.Clamp(target.y, -threshold + player.position.y, threshold + player.position.y);
         //mousePos = -mousePos;
-        transform.position = new Vector3(mousePos.x, mousePos.y, 0);
+        transform.position = mousePos;
     }
     public Quaternion GetRotationInfo()
     {
