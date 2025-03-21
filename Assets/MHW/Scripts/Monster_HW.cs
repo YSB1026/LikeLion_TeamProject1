e6a4d4971df986c42bf5345892cca801fdc32884
devUn9 <hyeonungmoon@gmail.com>
2025-03-21 16:05:47 +0900

    25/03/21 몬스터, 경험치드랍

--- Assets/MHW/Scripts/Monster_HW.cs
+++ Assets/MHW/Scripts/Monster_HW.cs
@@ -1,24 +1,32 @@
 using System.Collections;
+using System.ComponentModel;
+using System.Threading;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 public class Monster_HW : Character
 {
     public GameObject player;
     public float speed = 1.5f;
+    public GameObject[] expOrb;
+
+    private bool isDeath = false;
 
     private SpriteRenderer spriteRenderer;
     Animator animator;
-
-
+    
     protected override void Death()
     {
         animator.SetBool("isDeath", true);
-        Destroy(gameObject, 0.7f);
-    }
+        isDeath = true;
+        StartCoroutine(ReturnToPoolAfterDelay(0.7f)); // 0.7초후 풀반환
 
+        CreateExpOrb();
+
+    }
     protected override void Move()
     {
-        if (player != null)
+        if (isDeath == false) // 몬스터가 살아있을 때만
         {
             // 플레이어의 위치로 이동
             Vector3 direction = (player.transform.position - transform.position).normalized;
@@ -29,6 +37,13 @@ public class Monster_HW : Character
         }
     }
 
+    // delay 시간 이후 오브젝트 풀에 리턴하는 코루틴
+    private IEnumerator ReturnToPoolAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        PoolManager.Instance.Return(gameObject);
+    }
+
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
@@ -37,17 +52,37 @@ public class Monster_HW : Character
 
     void Update()
     {
-        Move();
-        
+        Move();    
     }
-
     
-
-    void OnCollisionEnter2D(Collision2D collision)
+    // 충돌처리, 추후 플레이어 공격
+    void OnTriggerEnter2D(Collider2D collider)
     {
-        if (collision.gameObject == player)
+        if (collider.gameObject.CompareTag("Player"))
         {
             Death();
         }
     }
+
+    void CreateExpOrb()
+    {
+        GameObject expOrbInstance;
+        if (gameObject.CompareTag("Monster1"))
+        {
+            expOrbInstance = Instantiate(expOrb[0], gameObject.transform.position, Quaternion.identity);
+            Destroy(expOrbInstance, 5f);
+        }
+        else if (gameObject.CompareTag("Monster2"))
+        {
+            expOrbInstance = Instantiate(expOrb[1], gameObject.transform.position, Quaternion.identity);
+            Destroy(expOrbInstance, 5f);
+        }
+        else
+        {
+            expOrbInstance = Instantiate(expOrb[2], gameObject.transform.position, Quaternion.identity);
+            Destroy(expOrbInstance, 5f);
+        }
+
+    }
+
 }
